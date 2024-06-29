1f3c4fa12e07023ef4cc9f12aa02b7fd6e9abca1
muhammadrezaulkarim <muhammadrezaulkarim>
2024-06-29 04:30:22 +0000

    dlt live table test read from azure blob storage using hive metastore

---
+++ data_live_table_test_read_from_azure_blob_Storage.py
@@ -0,0 +1,50 @@
+# Databricks notebook source
+import dlt
+from pyspark.sql.functions import *
+
+# COMMAND ----------
+
+storage_account_name = 'datastorageaccount760'
+storage_account_access_key = 'OM4+/m3W5myxo6RR7YsMMJhHwg7z6LWlWmD0cmkBUNcUFgcxez'
+spark.conf.set('fs.azure.account.key.' + storage_account_name + '.blob.core.windows.net', storage_account_access_key)
+
+# COMMAND ----------
+
+@dlt.table(
+  comment="Popular baby first names in New York. This data was ingested from the New York State Department of Health."
+)
+def baby_names_raw():
+  blob_container = 'baby-names'
+  filePath = "wasbs://" + blob_container + "@" + storage_account_name + ".blob.core.windows.net/Baby_Names__Beginning_2007.csv"
+  df = spark.read.format("csv").load(filePath, inferSchema = True, header = True)
+  df_renamed_column = df.withColumnRenamed("First Name", "First_Name")
+  return df_renamed_column
+
+# COMMAND ----------
+
+@dlt.table(
+  comment="New York popular baby first name data cleaned and prepared for analysis."
+)
+@dlt.expect("valid_first_name", "First_Name IS NOT NULL")
+@dlt.expect_or_fail("valid_count", "Count > 0")
+def baby_names_prepared():
+  return (
+    dlt.read("baby_names_raw")
+      .withColumnRenamed("Year", "Year_Of_Birth")
+      .select("Year_Of_Birth", "First_Name", "Count")
+  )
+
+# COMMAND ----------
+
+@dlt.table(
+  comment="A table summarizing counts of the top baby names for New York for 2021."
+)
+def top_baby_names_2021():
+  return (
+    dlt.read("baby_names_prepared")
+      .filter(expr("Year_Of_Birth == 2021"))
+      .groupBy("First_Name")
+      .agg(sum("Count").alias("Total_Count"))
+      .sort(desc("Total_Count"))
+      .limit(10)
+  )
